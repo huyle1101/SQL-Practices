0c918a745c7e461c58e9d5f5dcf5aa3cac56715f
Huy Le <241124029221@due.udn.vn>
2026-04-28 08:45:51 +0700

    Time: 193 ms (46.28%) | Memory: 0B (100.00%) - LeetSync

--- 262-trips-and-users/trips-and-users.sql
+++ 262-trips-and-users/trips-and-users.sql
@@ -8,8 +8,8 @@ WITH sub AS ( -- remove banned users and flag cancelled requests
         request_at,
         CASE WHEN status LIKE 'cancelled%' THEN 1 ELSE 0 END AS is_cancelled
     FROM trips t
-    JOIN users u1 ON t.client_id = u1.users_id AND u1.banned='No'
-    JOIN users u2 ON t.driver_id = u2.users_id AND u2.banned='No'
+    JOIN users u1 ON t.client_id = u1.users_id AND u1.banned='No' --filter out unbanned client
+    JOIN users u2 ON t.driver_id = u2.users_id AND u2.banned='No' --filter out unbanned drivers
     AND request_at::DATE BETWEEN '2013-10-01' AND '2013-10-03'
 )
 
