3f4032d3918dcf11bb786fa0fcbc5da2ac788ca7
Huy Le <241124029221@due.udn.vn>
2026-04-29 11:10:10 +0700

    Time: 291 ms (71.62%) | Memory: 0B (100.00%) - LeetSync

---
+++ 2024-calculate-special-bonus/calculate-special-bonus.sql
@@ -0,0 +1,7 @@
+-- Write your PostgreSQL query statement below
+SELECT
+ employee_id,
+ CASE 
+    WHEN employee_id%2!=0 AND name NOT LIKE 'M%' THEN salary ELSE 0 END AS bonus
+FROM employees
+ORDER BY 1
